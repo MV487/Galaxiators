6bdd029321bdf50cd43b8693f39b81e3814cc8c1
vijayashankarmohith2 <68358643+MV487@users.noreply.github.com>
2024-02-27 20:09:44 -0800

    update README.md

--- Galaxiators/src/main.cpp
+++ Galaxiators/src/main.cpp
@@ -5,20 +5,23 @@
 int main()
 {
 	constexpr char WINDOW_TITLE[] = "Galaxiators";
-	constexpr int WINDOW_WIDTH = 1080;
-	constexpr int WINDOW_HEIGHT = 1090;
+	constexpr int WINDOW_WIDTH = 1920;
+	constexpr int WINDOW_HEIGHT = 1080;
+	
+	std::cout << GetMonitorCount();
 
-	InitWindow(1080, 1090, WINDOW_TITLE);
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 	SetTargetFPS(60);
-
+	GetWindowScaleDPI();
 
 	while (!WindowShouldClose())
 	{
-
+	
 		BeginDrawing();
+
 		ClearBackground(BLACK);
 
-		DrawText("Galaxiators", WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 32, WHITE);
+		DrawText("Galaxiators", WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 50, WHITE);
 
 		EndDrawing();
 	}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Galaxiators
 space game
+
+## Install guide
+
+### Windows:
+```bash
+    git clone --recurse-submodules https://github.com/MV487/PongOnRoids.git
+    cd vcpkg\bootstrap-vcpkg.bat
+    cd vcpkg
+    ./bootstrap-vcpkg.bat
+    cd ..
+    \vcpkg\vcpkg.exe install 
+```
